1684238eb8371f5b4e7e69374a856c00e7d4a220
Daniel769 <jeam.nieto769@gmail.com>
2024-03-28 13:34:50 -0500

    (frontend): add all rows and black color prop

--- 04-lab-react-native-with-cli-calculator/src/components/CalculatorButton.tsx
+++ 04-lab-react-native-with-cli-calculator/src/components/CalculatorButton.tsx
@@ -5,17 +5,22 @@ import { styles, colors } from '../../config/theme/app-theme';
 interface props{
     label : string;
     color? : string;
+    doubleSize? : boolean;
+    blackText? : boolean;
 }
 
-export const CalculatorButton = ({label,color = colors.darkGray}:props) =>{
+export const CalculatorButton = ({label,color = colors.darkGray, doubleSize = false, blackText = false}:props) =>{
     return(
         <Pressable style={({pressed}) =>({
             ...styles.button,
             backgroundColor: color,
+            width : (doubleSize) ? 180 : 80,
             opacity: (pressed) ? 0.8 :1
 
         })}>
-        <Text style={styles.buttonText}>{label}</Text>
+        <Text style={{...styles.buttonText, color: (blackText)? 'black' : 'white'}}>
+            {label}
+            </Text>
     </Pressable>
     );
 };
