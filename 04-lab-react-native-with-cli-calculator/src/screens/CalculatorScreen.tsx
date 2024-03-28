09a6ef9065bf98e1aec0f2030be1c92ba1e49699
Daniel769 <jeam.nieto769@gmail.com>
2024-03-28 00:04:50 -0500

    (frontend):add styles, calculatorScreen, and first button

---
+++ 04-lab-react-native-with-cli-calculator/src/screens/CalculatorScreen.tsx
@@ -0,0 +1,22 @@
+import { Pressable, Text, View } from "react-native";
+import { styles } from "../../config/theme/app-theme";
+
+
+export const CalculatorScreen = () =>{
+    return(
+        <View style={styles.calculatorContainer}>
+            <View style={{paddingHorizontal: 30, paddingBottom:20}}>
+            <Text style={styles.mainResult}>1103</Text>
+            <Text style={styles.subResult}>11</Text>
+            </View>
+
+            <View>
+            <Pressable style={styles.button}>
+                <Text>AC</Text>
+            </Pressable>
+        </View> 
+        </View>
+
+
+    );
+};
